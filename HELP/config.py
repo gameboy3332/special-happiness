4e9c08df9f4450bf0a3beedd1d7477653b1b716f
gameboy3332 <166290816+gameboy3332@users.noreply.github.com>
2024-05-27 14:00:17 +0800

    Delete HELP directory

--- HELP/config.py
+++
@@ -1,8 +0,0 @@
-BINANCE_API_KEY = "hjVCDOITITxwv7bCIZWrUcm4P7nsaJkFirRnrqS8XtB12FyW05MmPJDLaSsz5Cl7"
-BINANCE_SECRET_KEY = "VZ9LPoYlxcqZXLBSbvm534nHnZ9zf53ra8eAfEiAgPIhSpcRxfdK88Seuesj8esI"
-
-
-OPENAI_API_KEY = "sk-proj-unw8L8H3qdb8g76alnOvT3BlbkFJVVAFMdLPK3gKpXZPHPXW"
-
-CSV_PATH = "C:\\Users\\柳柳柳柳柳\\PycharmProjects\\b\\BMI計算機\\.venv\\HELP\\aaa"
-OTHER_FILES_PATH = "C:\\Users\\柳柳柳柳柳\\PycharmProjects\\b\\BMI計算機\\.venv\\HELP\\bbb"
